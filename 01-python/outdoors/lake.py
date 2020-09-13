3745fd4fc076acc9c124bb5b4cdacac4f114b3e4
Gabriel Klavans <gabeklav@bu.edu>
2020-09-13 18:03:52 -0400

    Created a beautiful lake for the community with variable amounts of fish

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,3 +1,36 @@
-def draw_lake():
-    print("lake not found")
-    return
+from random import random
+
+def draw_lake(numFish=3):
+  # Make sure user didn't put in a number that was too high
+  numFish = 8 if numFish > 8 else numFish
+  # Same for too low
+  numFish = 0 if numFish < 0 else numFish
+  
+  total_fish = 8
+  fish = []
+  for i in range(numFish):
+    if random() > 0.5:
+      fish.append("<O")
+    else:
+      fish.append("O>")
+
+  for i in range(total_fish-numFish):
+    fish.append("  ")
+
+  print("""
+     ____^-------       _____________________
+    / ---------\ \     / ------------------- \ 
+   / /          \ \___/ /       %s          \ \_
+  | |    %s      \ ----/                      | |
+  | |                          ^-^-^          | |
+  | |  ^-^-^                   %s             | |
+   \ \             %s                         | |
+    \ \                               %s     /  /
+     \ \____           ^-^-^                /  /
+       -----  \              %s             | |
+             \ \   %s                        \ \ 
+              \ \           ^-^-^   %s     /-___|
+              | |__________     __________/ /
+              \ ____________/^\ ___________/
+  """ % tuple(fish))        
+  return
